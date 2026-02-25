cf8641ca67beaecc62e1fc9c1087d455b19d62f4
Rahulk0909 <rahulyadav19845@gmail.com>
2026-02-25 13:54:50 +0530

    js

--- JAVA_SCRIPT/script.js
+++ JAVA_SCRIPT/script.js
@@ -1,2 +1,21 @@
 console.log("Hello, World!");
-document.write( "Hello World !");
+// document.write( "Hello World !");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
+num = "hello";
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
