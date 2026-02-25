3853af28c492dfcc6948502ef870d402fd202f6f
Rahulk0909 <rahulyadav19845@gmail.com>
2026-02-25 14:40:14 +0530

    js

---
+++ JAVA_SCRIPT/CLASS/operator.js
@@ -0,0 +1,7 @@
+var a = 100;
+var b = 13;
+var c = 10;
+var c = a + b;
+console.log(c);
+
+
--- JAVA_SCRIPT/CLASS/script.js
+++ JAVA_SCRIPT/CLASS/script.js
@@ -1,4 +1,4 @@
-console.log("Hello, World!");
+/*console.log("Hello, World!");
 // document.write( "Hello World !");
 var num = 10;
 console.log(num);
@@ -18,4 +18,16 @@ console.log(typeof(num));
 
 num = undefined;
 console.log(num);
-console.log(typeof(num));
+console.log(typeof(num));*/
+
+
+const sym1 =Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1 == sym2){
+console.log("true");
+}
+else{
+console.log("false");
+}
