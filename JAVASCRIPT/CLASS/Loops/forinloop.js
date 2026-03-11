3a43b2948279d212535f98cddf6717f0d98f9117
Rahulk0909 <rahulyadav19845@gmail.com>
2026-03-11 13:56:56 +0530

    today

---
+++ JAVASCRIPT/CLASS/Loops/forinloop.js
@@ -0,0 +1,11 @@
+
+        const person = {
+            fname:"ajay",
+            iname:"singh",
+            age :25
+        };
+
+        for(let x in person){
+            console.log("person details"+x+":"+person[x])
+        }
+   
